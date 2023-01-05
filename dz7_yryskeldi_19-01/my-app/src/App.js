6c96787bfd21d02c5fe55b0ffe79f45786646f89
Yryskeldi007 <112939262+Yryskeldi007@users.noreply.github.com>
2023-01-05 23:56:09 +0600

    Add files via upload

---
+++ dz7_yryskeldi_19-01/my-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [ seconds, setSeconds ] = React.useState(60);
+  const [ timerActive, setTimerActive ] = React.useState(false);
+
+  React.useEffect(() => {
+    if (seconds > 0 && timerActive) {
+      setTimeout(setSeconds, 100, seconds - 1);
+    } else {
+      setTimerActive(false);
+    }
+  }, [ seconds, timerActive ]);
+
+  return (
+    <div>
+      {seconds
+        ? <React.Fragment>
+            <button onClick={() => setTimerActive(!timerActive)}>
+              {timerActive ? 'stop' : 'start'}
+            </button>
+            <div>{seconds}</div>
+          </React.Fragment>
+        : <button onClick={() => setSeconds(60)}>ещё раз</button>
+      }
+    </div>
+  );
+}
+export default App
